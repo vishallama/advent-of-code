e0e674c0a829096168bd579ea50dbc35402211ae
Vishal Lama <vishal.lama@gmail.com>
2017-12-01 16:19:12 -0700

    Write Node.js code for day 1 challenge

---
+++ 2017/src/nodejs/day01/inverseCaptcha.js
@@ -0,0 +1,22 @@
+// Day 1: Inverse Captcha
+const puzzleinput = require('./captcha.js');
+
+const str = puzzleinput.input.trim();
+const inputArray = str.split("").map(value => parseInt(value, 10));
+
+function inverseCaptcha(array, offset) {
+
+  const arrayLength = array.length;
+
+  return array.reduce((acc, curr, index, arr) => {
+    if (arr[index] === arr[(index + offset) % arrayLength]) {
+      return acc + curr;
+    } else {
+      return acc;
+    }
+  }, 0);
+}
+
+// Tests
+console.log(inverseCaptcha(inputArray, 1));                             // 1251
+console.log(inverseCaptcha(inputArray, inputArray.length / 2));         // 1244
